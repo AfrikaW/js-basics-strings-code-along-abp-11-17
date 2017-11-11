9a8bf7571004a8d24349b910debb382e08526bfe
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-11 00:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser= 'Khole Williams';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser} !`
+currentUser.slice(0, 1)
